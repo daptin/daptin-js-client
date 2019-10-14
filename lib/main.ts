abb5dfb5a6bb02856b5fc2dbaf179fad8d15785e
artpar <artpar@gmail.com>
2019-10-14 17:06:04 +0530

    user can inject their tokenGetter interface function for multiple instances of daptin-client in same env

--- lib/main.ts
+++ lib/main.ts
@@ -7,49 +7,49 @@ import {TokenGetter} from "./clients/interface";
 const JsonApi = require('devour-client')
 
 class LocalStorageTokenGetter {
-  getToken(): string {
-    return localStorage.getItem("token")
-  }
+    getToken(): string {
+        return localStorage.getItem("token")
+    }
 }
 
 export class DaptinClient {
 
-  appConfig: AppConfig;
-  jsonApi: any;
-  tokenGetter: TokenGetter;
-  actionManager: ActionManager;
-  worldManager: WorldManager;
-  statsManager: StatsManager;
+    appConfig: AppConfig;
+    jsonApi: any;
+    tokenGetter: TokenGetter;
+    actionManager: ActionManager;
+    worldManager: WorldManager;
+    statsManager: StatsManager;
 
-  constructor(endpoint, debug) {
-    const that = this;
-    debug = debug || false;
-    that.appConfig = new AppConfig(endpoint);
+    constructor(endpoint, debug, tokenGetter) {
+        const that = this;
+        debug = debug || false;
+        that.appConfig = new AppConfig(endpoint);
 
-    that.jsonApi = new JsonApi({
-      apiUrl: that.appConfig.getEndpoint() + '/api',
-      pluralize: false,
-      logger: debug
-    });
+        that.jsonApi = new JsonApi({
+            apiUrl: that.appConfig.getEndpoint() + '/api',
+            pluralize: false,
+            logger: debug
+        });
 
-    that.tokenGetter = new LocalStorageTokenGetter();
-    that.actionManager = new ActionManager(that.appConfig, that.tokenGetter);
-    that.worldManager = new WorldManager(that.appConfig, that.tokenGetter, that.jsonApi, that.actionManager)
-    that.statsManager = new StatsManager(that.appConfig, that.tokenGetter)
-
-
-    that.jsonApi.insertMiddlewareBefore("HEADER", {
-      name: "Auth Header middleware",
-      req: function (req) {
-        let token = that.tokenGetter.getToken();
-        if (token) {
-          that.jsonApi.headers['Authorization'] = 'Bearer ' + token;
-        }
-        return req
-      }
-    });
+        that.tokenGetter = tokenGetter || new LocalStorageTokenGetter();
+        that.actionManager = new ActionManager(that.appConfig, that.tokenGetter);
+        that.worldManager = new WorldManager(that.appConfig, that.tokenGetter, that.jsonApi, that.actionManager)
+        that.statsManager = new StatsManager(that.appConfig, that.tokenGetter)
 
 
-  }
+        that.jsonApi.insertMiddlewareBefore("HEADER", {
+            name: "Auth Header middleware",
+            req: function (req) {
+                let token = that.tokenGetter.getToken();
+                if (token) {
+                    that.jsonApi.headers['Authorization'] = 'Bearer ' + token;
+                }
+                return req
+            }
+        });
+
+
+    }
 
 }
